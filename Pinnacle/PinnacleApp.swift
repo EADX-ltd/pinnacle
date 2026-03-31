fd580ba460c7d78378f91e8afcd60f821c04e29a
zhivko <zpv@eadx.com>
2026-03-31 10:14:38 +0300

    Initial Commit

---
+++ Pinnacle/PinnacleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinnacleApp.swift
+//  Pinnacle
+//
+//  Created by Zhivko Poroyliev on 31.03.26.
+//
+
+import SwiftUI
+
+@main
+struct PinnacleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
